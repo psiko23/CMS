050ed33e4e036cc0f8d1643b786a3e9dcb7f45d1
Psiko23 <tylerpittman2001@gmail.com>
2024-01-16 16:44:37 -0800

    updated logic for application

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
-const getUserInput = require('./lib/get_input');
+const viewAllDepartments = require('./lib/queries/view_all_departments');
+const viewAllRoles = require('./lib/queries/view_all_roles');
+const viewAllEmployees = require('./lib/queries/view_all_employees');
+const addDepartment = require('./lib/queries/add_department');
+const addEmployee = require('./lib/queries/add_employee');
+const addRole = require('./lib/queries/add_role');
 
 
+const inquirerFunction = require('./lib/inquirer_functions');
+
+const startApp = async () => {
+    
+    const query = new inquirerFunction('action', 
+                     'list', 
+                     'What would you like to do?',
+
+                     ['View all departments', 
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add an employee',
+                    'Update an employee role',
+                    'Exit',]
+    );
+    const answer = await query.ask();
+    const action = answer.action;
+
+    if(action === 'View all departments') {
+        await viewAllDepartments();
+    }
+    if(action === 'View all roles') {
+        await viewAllRoles();
+    }
+    if(action === 'View all employees') {
+        await viewAllEmployees();
+    }
+    if(action === 'Add a department') {
+        await addDepartment();
+    }
+    if(action === 'Add a role') {
+        await addRole();
+    }
+    if(action === 'Add an employee') {
+        await addEmployee();
+    }
+    startApp();
+}
+
+startApp();
+
+
+
+// const runApp = async () => {
+//     const answer = await startApp.ask();
+//     const action = answer.action;
+
+//     if (action === 'View all departments') {
+//         await viewAllDepartments();
+//     }
+//     runApp();
+// }
+
+// runApp();
